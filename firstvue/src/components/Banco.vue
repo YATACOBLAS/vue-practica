aac8fe168edf62a31b566533899049a9876ee107
YATACOBLAS <juan_blas_1905@hotmail.com>
2020-12-18 11:48:57 -0500

    Primera prueba Vue

--- firstvue/src/components/Banco.vue
+++ firstvue/src/components/Banco.vue
@@ -1,44 +1,58 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h1>Tipo de Cuenta : {{cuenta}}</h1>
-    <h1 :style="[ (saldo<=500) ? 'color:red':'color:black']"> Saldo : {{ saldo}}</h1>
-         <div v-if="Activo" >Cuenta Activa</div>
-         <div v-else> Cuenta Inactiva</div>
-    <div v-for="(tarjeta, index) in tarjetas" :key="index" > 
-      {{index + 1}}- {{tarjeta}}
+    <h1>Tipo de Cuenta : {{ cuenta }}</h1>
+    <h1 :style="color">Saldo : {{ saldo }}</h1>
+    <h2>Cuenta {{ estado ? "Activa" : "Inactiva" }}</h2>
+
+    <div v-for="(tarjeta, index) in tarjetas" :key="index">
+      {{ index + 1 }}- {{ tarjeta }}
     </div>
-   </div>
+    <Button @action="aumentar" msjboton="Aumetar Saldo" />
+    <Button @action="disminuir" msjboton="Disminuir Saldo" :deshabilitar="deshabilitar" />
+  </div>
 </template>
 
 <script>
-import Button from './Button.vue'
+import Button from "./Button.vue";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  components: { Button },
   props: {
-    msg: String
+    msg: String,
   },
-  data(){
-    return{
-     cuenta:'Visa',
-     saldo:'1000',
-     Activo:true,
-     tarjetas:['Visa','MasterCard','BCP']
-    }
+  data() {
+    return {
+      cuenta: "Visa",
+      deshabilitar:false,
+      saldo: 1000,
+      estado: true,
+      tarjetas: ["Visa", "MasterCard", "BCP"],
+    };
   },
   methods: {
-      aumentar(){
-        this.saldo = this.saldo+100;
-      },
-      disminuir (){
-        if(this.saldo===0){
-          this.Activo=false;
-          return
+    aumentar() {
+      this.saldo = this.saldo + 100;
+      this.estado = true;
+      this.deshabilitar=false;
+      return 
+    },
+    disminuir() {
+      if (this.saldo > 0) {
+          this.saldo = this.saldo - 100;
+        if (this.saldo === 0) {
+          this.estado = false;
+            this.deshabilitar=true
+          return ;
         }
-        this.saldo=this.saldo - 100;
+        return;
       }
+    },
+    color() {
+      return this.saldo <= 500 ? "color:red" : "color:black";
+    },
   },
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- firstvue/src/components/Button.vue
+++ firstvue/src/components/Button.vue
@@ -1,15 +1,21 @@
 <template>
     <div>
-        <button  >{{msjboton}}</button>
+        <button @click="accionar"  :disabled="deshabilitar">{{msjboton}}</button>
     </div>
 </template>
 
 <script>
     export default {
     name:'Button',
-    props:[msjboton],
+    props:{msjboton:String,
+    deshabilitar:{
+        type:Boolean,
+        default:false
+    } },
     methods: {
-        
+        accionar(){
+            this.$emit('action')
+        }
     },
     }
 </script>
